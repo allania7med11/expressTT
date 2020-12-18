2863eb1f588e1f81d4f6f200d50b6b6e1ef3a6a7
ahmedallani <allania7med1@gmail.com>
2020-12-18 14:17:36 +0100

    proxt

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/api/test", function(req, res, next) {
-  res.send({ proxy: true });
+  res.send({ proxy: "message from the server using proxy" });
 });
 
 module.exports = router;
